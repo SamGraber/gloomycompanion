35980890a378130d9c9dbf17a7fe746213b4744c
Sam Graber <authorsamgraber@gmail.com>
2018-06-20 11:38:32 -0400

    Convert deck and scenario list to classes
    These are a little easier to reason about than the class-like functions that were used here.

--- app/deck-list.ts
+++ app/deck-list.ts
@@ -6,61 +6,66 @@ declare var is_checked;
 declare var input_value;
 declare var concat_arrays;
 
-function DeckList() {
-    var decklist = {} as any;
-    decklist.ul = document.createElement('ul');
-    decklist.ul.className = 'selectionlist';
-    decklist.checkboxes = {};
-    decklist.level_selectors = {};
-    decklist.global_level_selector = null;
+class DeckList {
+    ul: HTMLUListElement;
+    checkboxes = {} as any;
+    level_selectors = {} as any;
+    global_level_selector = null as any;
+
+    constructor() {
+        this.ul = document.createElement('ul');
+        this.ul.className = 'selectionlist';
+        this.checkboxes = {};
+        this.level_selectors = {};
+        this.global_level_selector = null;
 
 
-    var listitem = document.createElement('li');
-    var global_level_selector = new LevelSelector('Select global level ', true);
-    listitem.appendChild(global_level_selector.html);
-    decklist.global_level_selector = global_level_selector;
-
-    var dom_dict = create_button('button', 'applylevel', 'Apply All');
-    dom_dict.onclick = function () {
-        for (const key in decklist.level_selectors) {
-            decklist.level_selectors[key].set_value(decklist.global_level_selector.get_selection());
-        }
-    };
-    listitem.appendChild(dom_dict);
-
-    decklist.ul.appendChild(listitem);
-
-    for (const key in DECKS) {
-        var real_name = DECKS[key].name;
         var listitem = document.createElement('li');
-        var dom_dict = create_input('checkbox', 'deck', real_name, real_name);
-        listitem.appendChild(dom_dict.root);
+        var global_level_selector = new LevelSelector('Select global level ', true);
+        listitem.appendChild(global_level_selector.html);
+        this.global_level_selector = global_level_selector;
 
-        var level_selector = new LevelSelector(' with level ', true);
-        listitem.appendChild(level_selector.html);
+        var dom_dict = create_button('button', 'applylevel', 'Apply All');
+        dom_dict.onclick = () => {
+            for (const key in this.level_selectors) {
+                this.level_selectors[key].set_value(this.global_level_selector.get_selection());
+            }
+        };
+        listitem.appendChild(dom_dict);
 
-        decklist.ul.appendChild(listitem);
-        decklist.checkboxes[real_name] = dom_dict.input;
-        decklist.level_selectors[real_name] = level_selector;
+        this.ul.appendChild(listitem);
 
+        for (const key in DECKS) {
+            var real_name = DECKS[key].name;
+            var listitem = document.createElement('li');
+            var dom_dict = create_input('checkbox', 'deck', real_name, real_name);
+            listitem.appendChild(dom_dict.root);
+
+            var level_selector = new LevelSelector(' with level ', true);
+            listitem.appendChild(level_selector.html);
+
+            this.ul.appendChild(listitem);
+            this.checkboxes[real_name] = dom_dict.input;
+            this.level_selectors[real_name] = level_selector;
+
+        }
     }
-    ;
 
-    decklist.get_selection = function () {
+    get_selection() {
         return dict_values(this.checkboxes).filter(is_checked).map(input_value);
     }
 
-    decklist.get_selected_decks = function () {
+    get_selected_decks() {
         var selected_checkbox = this.get_selection();
         var selected_decks = concat_arrays(selected_checkbox.map(function (name) {
             var deck = ((name in DECKS) ? DECKS[name] : []);
-            deck.level = decklist.level_selectors[name].get_selection();
+            deck.level = this.level_selectors[name].get_selection();
             return deck;
         }.bind(this)));
         return selected_decks;
     }
 
-    decklist.set_selection = function (selected_deck_names) {
+    set_selection(selected_deck_names) {
         dict_values(this.checkboxes).forEach(function (checkbox) {
             checkbox.checked = false;
         });
@@ -69,10 +74,8 @@ function DeckList() {
             var checkbox = this.checkboxes[deck_names.name];
             if (checkbox) {
                 checkbox.checked = true;
-                decklist.level_selectors[deck_names.name].set_value(deck_names.level);
+                this.level_selectors[deck_names.name].set_value(deck_names.level);
             }
         }.bind(this));
     }
-
-    return decklist;
 }
--- app/scenario-list.ts
+++ app/scenario-list.ts
@@ -2,44 +2,47 @@ declare var create_input;
 declare var SPECIAL_RULES;
 declare var DECKS;
 
-function ScenarioList(scenarios) {
-    var scenariolist = {} as any;
-    scenariolist.ul = document.createElement('ul');
-    scenariolist.ul.className = 'selectionlist';
-    scenariolist.spinner = null;
-    scenariolist.decks = {};
-    scenariolist.special_rules = {};
-    scenariolist.level_selector = null;
+class ScenarioList {
+    ul: HTMLUListElement;
+    spinner: HTMLInputElement;
+    decks = {} as any;
+    special_rules = {} as any;
+    level_selector: LevelSelector;
 
-    scenariolist.level_selector = new LevelSelector('Select level', false);
-
-    scenariolist.ul.appendChild(scenariolist.level_selector.html);
-
-    for (var i = 0; i < scenarios.length; i++) {
-        var scenario = scenarios[i];
-        scenariolist.decks[i] = scenario.decks;
-        scenariolist.special_rules[i] = scenario.special_rules ? scenario.special_rules : '';
+    constructor(private scenarios) {
+        this.ul = document.createElement('ul');
+        this.ul.className = 'selectionlist';
+    
+        this.level_selector = new LevelSelector('Select level', false);
+    
+        this.ul.appendChild(this.level_selector.html);
+    
+        for (var i = 0; i < scenarios.length; i++) {
+            var scenario = scenarios[i];
+            this.decks[i] = scenario.decks;
+            this.special_rules[i] = scenario.special_rules ? scenario.special_rules : '';
+        }
+    
+        var listitem = document.createElement('li');
+        listitem.innerText = 'Select scenario number';
+        this.ul.appendChild(listitem);
+    
+        var scenario_spinner = create_input('number', 'scenario_number', '1', '');
+        scenario_spinner.input.min = 1;
+        scenario_spinner.input.max = scenarios.length;
+        this.ul.appendChild(scenario_spinner.input);
+        this.spinner = scenario_spinner.input;
     }
 
-    var listitem = document.createElement('li');
-    listitem.innerText = 'Select scenario number';
-    scenariolist.ul.appendChild(listitem);
-
-    var scenario_spinner = create_input('number', 'scenario_number', '1', '');
-    scenario_spinner.input.min = 1;
-    scenario_spinner.input.max = scenarios.length;
-    scenariolist.ul.appendChild(scenario_spinner.input);
-    scenariolist.spinner = scenario_spinner.input;
-
-    scenariolist.get_selection = function () {
+    get_selection() {
         // We're using the scenario index that is zero-based, but the scenario list is 1-based
-        var current_value = scenariolist.spinner.value - 1;
-        return Math.min(current_value, scenarios.length + 1);
+        var current_value = +this.spinner.value - 1;
+        return Math.min(current_value, this.scenarios.length + 1);
     }
 
-    scenariolist.get_level = function (deck_name, special_rules) {
+    get_level(deck_name, special_rules) {
 
-        var base_level = scenariolist.level_selector.get_selection();
+        var base_level = this.level_selector.get_selection();
 
         if ((special_rules.indexOf(SPECIAL_RULES.living_corpse_two_levels_extra) >= 0) && (deck_name == SPECIAL_RULES.living_corpse_two_levels_extra.affected_deck)) {
             return Math.min(7, (parseInt(base_level) + parseInt(SPECIAL_RULES.living_corpse_two_levels_extra.extra_levels)));
@@ -48,21 +51,19 @@ function ScenarioList(scenarios) {
         }
     }
 
-    scenariolist.get_scenario_decks = function () {
+    get_scenario_decks() {
         return (this.decks[this.get_selection()].map(function (deck) {
             if (DECKS[deck.name]) {
                 deck.class = DECKS[deck.name].class;
             } else if (deck.name.indexOf('Boss') != -1) {
                 deck.class = DECKS['Boss'].class;
             }
-            deck.level = scenariolist.get_level(deck.name, scenariolist.get_special_rules());
+            deck.level = this.get_level(deck.name, this.get_special_rules());
             return deck;
         }));
     }
 
-    scenariolist.get_special_rules = function () {
+    get_special_rules() {
         return this.special_rules[this.get_selection()];
     }
-
-    return scenariolist;
 }
