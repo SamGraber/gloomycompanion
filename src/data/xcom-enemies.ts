0091213f512096b016156ab9449c5911ff5a4065
SamGraber <authorsamgraber@gmail.com>
2018-06-21 15:01:53 -0400

    Shorten the monster stats storage format
    I want to make it simpler to create new monsters without having to fill out so much data. To do this I created a new format where you specify the base stats of the unit, and then a list of upgrades for each level (for normal and elite, plus one 'base' upgrade for level 0 elites). The monster service then 'denormalizes' the data by marking up the monster stats by the upgrade amount for each level to populate the complete list of levels.
    
    This shortens the format by only requiring you to specify what changes at each level. Note that if there are any changes to the attributes, you will need to respecify the full list of attributes. It would be too difficult to figure out how to splice the upgrades together with the previous attributes.

--- src/data/xcom-enemies.ts
+++ src/data/xcom-enemies.ts
@@ -1,151 +1,99 @@
 export const XCOMEnemies = {
 	'Advent Trooper': {
-		'level': [
-		{
-			'level': 0,
-			'normal': {
-				'health': 4,
-				'move': 2,
-				'attack': 2,
-				'range': 3,
-				'attributes': []
-			},
-			'elite': {
-				'health': 6,
-				'move': 2,
-				'attack': 3,
-				'range': 3,
-				'attributes': []
-			}
+		base: {
+			'health': 4,
+			'move': 2,
+			'attack': 2,
+			'range': 3,
+			'attributes': []
 		},
-		{
-		'level': 1,
-			'normal': {
-				'health': 5,
-				'move': 2,
-				'attack': 2,
-				'range': 4,
-				'attributes': []
+		upgrades: [
+			{
+				level: 1,
+				type: 'normal',
+				health: 1,
+				range: 1,
 			},
-			'elite': {
-				'health': 7,
-				'move': 2,
-				'attack': 3,
-				'range': 5,
-				'attributes': []
-			}
-		},
-		{
-		'level': 2,
-			'normal': {
-				'health': 6,
-				'move': 3,
-				'attack': 2,
-				'range': 4,
-				'attributes': []
+			{
+				level: 2,
+				type: 'normal',
+				health: 1,
+				move: 1,
 			},
-			'elite': {
-				'health': 9,
-				'move': 3,
-				'attack': 3,
-				'range': 5,
-				'attributes': []
-			}
-		},
-		{
-		'level': 3,
-			'normal': {
-				'health': 6,
-				'move': 3,
-				'attack': 3,
-				'range': 4,
-				'attributes': []
+			{
+				level: 3,
+				type: 'normal',
+				attack: 1,
 			},
-			'elite': {
-				'health': 10,
-				'move': 3,
-				'attack': 4,
-				'range': 5,
-				'attributes': []
-			}
-		},
-		{
-		'level': 4,
-			'normal': {
-				'health': 8,
-				'move': 3,
-				'attack': 3,
-				'range': 4,
-				'attributes': []
+			{
+				level: 4,
+				type: 'normal',
+				health: 2,
 			},
-			'elite': {
-				'health': 10,
-				'move': 3,
-				'attack': 4,
-				'range': 6,
-				'attributes': [
-				'%poison%'
-				]
-			}
-		},
-		{
-		'level': 5,
-			'normal': {
-				'health': 10,
-				'move': 3,
-				'attack': 3,
-				'range': 5,
-				'attributes': []
+			{
+				level: 5,
+				type: 'normal',
+				health: 2,
+				range: 1,
 			},
-			'elite': {
-				'health': 12,
-				'move': 4,
-				'attack': 4,
-				'range': 6,
-				'attributes': [
-				'%poison%'
-				]
-			}
-		},
-		{
-		'level': 6,
-			'normal': {
-				'health': 10,
-				'move': 3,
-				'attack': 4,
-				'range': 5,
-				'attributes': []
+			{
+				level: 6,
+				type: 'normal',
+				attack: 1,
 			},
-			'elite': {
-				'health': 13,
-				'move': 4,
-				'attack': 5,
-				'range': 6,
-				'attributes': [
-				'%poison%'
-				]
-			}
-		},
-		{
-		'level': 7,
-			'normal': {
-				'health': 13,
-				'move': 3,
-				'attack': 4,
-				'range': 5,
-				'attributes': []
+			{
+				level: 7,
+				type: 'normal',
+				health: 3,
 			},
-			'elite': {
-				'health': 17,
-				'move': 4,
-				'attack': 5,
-				'range': 6,
-				'attributes': [
-				'%poison%'
-				]
-			}
-		}
-	]
+			{
+				level: 0,
+				type: 'elite',
+				health: 2,
+				attack: 1,
+				range: 1,
+			},
+			{
+				level: 1,
+				type: 'elite',
+				health: 1,
+				range: 1,
+			},
+			{
+				level: 2,
+				type: 'elite',
+				health: 2,
+				move: 1,
+			},
+			{
+				level: 3,
+				type: 'elite',
+				health: 1,
+				attack: 1,
+			},
+			{
+				level: 4,
+				type: 'elite',
+				range: 1,
+				attributes: ['%poison%'],
+			},
+			{
+				level: 5,
+				type: 'elite',
+				health: 2,
+				move: 1,
+			},
+			{
+				level: 6,
+				type: 'elite',
+				health: 1,
+				attack: 1,
+			},
+			{
+				level: 7,
+				type: 'elite',
+				health: 4,
+			},
+		],
 	}
-  };
-  
+};
--- src/monsters/monsters.service.ts
+++ src/monsters/monsters.service.ts
@@ -1,14 +1,94 @@
 import { MONSTER_STATS } from '../data/gloomhaven-monsters';
 import { XCOMEnemies } from '../data/xcom-enemies';
 
+interface IMonsterCollection {
+	monsters: { [key: string]: IMonster };
+	bosses: { [key: string]: any };
+}
+
+interface IMonster {
+	level: {
+		level: number;
+		normal: IMonsterStats;
+		elite: IMonsterStats;
+	}[];
+	base?: IMonsterStats;
+	upgrades?: {
+		level: number;
+		type: 'normal' | 'elite';
+		health: number;
+		move: number;
+		attack: number;
+		range: number;
+		attributes: string[];
+	}[];
+}
+
+interface IMonsterStats {
+	health: number;
+	move: number;
+	attack: number;
+	range: number;
+	attributes: string[];
+}
+
 class MonstersService {
-	monsters = {
-		monsters: {
-			...MONSTER_STATS.monsters,
-			...XCOMEnemies,
-		},
-		bosses: MONSTER_STATS.bosses,
-	};
+	monsters: IMonsterCollection;
+
+	constructor() {
+		this.monsters = {
+			monsters: {
+				...MONSTER_STATS.monsters,
+			},
+			bosses: MONSTER_STATS.bosses,
+		};
+		Object.keys(XCOMEnemies).forEach(key => {
+			this.monsters.monsters[key] = this.denormalizeMonsterStats(XCOMEnemies[key]);
+		});
+	}
+
+	private denormalizeMonsterStats(monster: Partial<IMonster>): IMonster {
+		if (monster.level) {
+			return monster as IMonster;
+		}
+
+		if (!monster.base || !monster.upgrades) {
+			throw new Error('You must define the base and upgrade stats or define the full stats sheet');
+		}
+		
+		const upgrades = monster.upgrades;
+		const newMonster: IMonster = {
+			level: [{
+				level: 0,
+				normal: monster.base,
+				elite: this.applyUpgrade(monster.base, upgrades.find(x => x.level === 0 && x.type === 'elite')),
+			}],
+		};
+		Array.from(new Array(7)).map((x, index) => index + 1).forEach(level => {
+			const previousLevel = newMonster.level.find(x => x.level === level - 1) as any;
+			const newLevel = {
+				level,
+				normal: this.applyUpgrade(previousLevel.normal, upgrades.find(x => x.level === level && x.type === 'normal')),
+				elite: this.applyUpgrade(previousLevel.elite, upgrades.find(x => x.level === level && x.type === 'elite')),
+			};
+			newMonster.level = [...newMonster.level, newLevel];
+		});
+		return newMonster;
+	}
+
+	private applyUpgrade(previousStats: IMonsterStats, upgrades: IMonsterStats | undefined): IMonsterStats {
+		if (!upgrades) {
+			return previousStats;
+		}
+
+		return {
+			health: upgrades.health ? previousStats.health + upgrades.health : previousStats.health,
+			move: upgrades.move ? previousStats.move + upgrades.move : previousStats.move,
+			attack: upgrades.attack ? previousStats.attack + upgrades.attack : previousStats.attack,
+			range: upgrades.range ? previousStats.range + upgrades.range : previousStats.range,
+			attributes: upgrades.attributes ? upgrades.attributes : previousStats.attributes,
+		};
+	}
 }
 
 export default new MonstersService();
