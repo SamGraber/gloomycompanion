21a5585c8923c239fa934ea3f17d3dc95ad09301
SamGraber <authorsamgraber@gmail.com>
2018-06-21 16:08:09 -0400

    Convert city data to upgrades syntax
    I wanted to see how the upgrades compare between bandit and city enemies.

--- src/data/gloomhaven/city.data.ts
+++ src/data/gloomhaven/city.data.ts
@@ -1,348 +1,208 @@
 export const CityEnemies = {
 	'City Archer': {
-		'level': [
-		  {
-			'level': 0,
-			'normal': {
-			  'health': 4,
-			  'move': 1,
-			  'attack': 2,
-			  'range': 3,
-			  'attributes': []
+		base: {
+			'health': 4,
+			'move': 1,
+			'attack': 2,
+			'range': 3,
+			'attributes': []
+		},
+		upgrades: [
+			{
+				level: 1,
+				type: 'normal',
+				health: 1,
+				range: 1,
 			},
-			'elite': {
-			  'health': 6,
-			  'move': 1,
-			  'attack': 3,
-			  'range': 4,
-			  'attributes': []
-			}
-		  },
-		  {
-			'level': 1,
-			'normal': {
-			  'health': 5,
-			  'move': 1,
-			  'attack': 2,
-			  'range': 4,
-			  'attributes': []
+			{
+				level: 2,
+				type: 'normal',
+				health: 1,
+				attack: 1,
 			},
-			'elite': {
-			  'health': 6,
-			  'move': 1,
-			  'attack': 3,
-			  'range': 5,
-			  'attributes': [
-				'%pierce% 1',
-				'%shield% 1'
-			  ]
-			}
-		  },
-		  {
-			'level': 2,
-			'normal': {
-			  'health': 6,
-			  'move': 1,
-			  'attack': 3,
-			  'range': 4,
-			  'attributes': []
+			{
+				level: 3,
+				type: 'normal',
+				move: 1,
+				attributes: ['%shield% 1'],
 			},
-			'elite': {
-			  'health': 7,
-			  'move': 1,
-			  'attack': 4,
-			  'range': 5,
-			  'attributes': [
-				'%pierce% 2',
-				'%shield% 1'
-			  ]
-			}
-		  },
-		  {
-			'level': 3,
-			'normal': {
-			  'health': 6,
-			  'move': 2,
-			  'attack': 3,
-			  'range': 4,
-			  'attributes': [
-				'%shield% 1'
-			  ]
+			{
+				level: 4,
+				type: 'normal',
+				health: 2,
+				range: 1,
 			},
-			'elite': {
-			  'health': 8,
-			  'move': 2,
-			  'attack': 4,
-			  'range': 5,
-			  'attributes': [
-				'%pierce% 2',
-				'%shield% 2'
-			  ]
-			}
-		  },
-		  {
-			'level': 4,
-			'normal': {
-			  'health': 8,
-			  'move': 2,
-			  'attack': 3,
-			  'range': 5,
-			  'attributes': [
-				'%shield% 1'
-			  ]
+			{
+				level: 5,
+				type: 'normal',
+				health: 1,
+				attack: 1,
 			},
-			'elite': {
-			  'health': 10,
-			  'move': 2,
-			  'attack': 4,
-			  'range': 6,
-			  'attributes': [
-				'%pierce% 2',
-				'%shield% 2'
-			  ]
-			}
-		  },
-		  {
-			'level': 5,
-			'normal': {
-			  'health': 9,
-			  'move': 2,
-			  'attack': 4,
-			  'range': 5,
-			  'attributes': [
-				'%shield% 1'
-			  ]
+			{
+				level: 6,
+				type: 'normal',
+				move: 1,
+				attributes: ['%shield% 2'],
 			},
-			'elite': {
-			  'health': 11,
-			  'move': 2,
-			  'attack': 5,
-			  'range': 6,
-			  'attributes': [
-				'%pierce% 3',
-				'%shield% 2'
-			  ]
-			}
-		  },
-		  {
-			'level': 6,
-			'normal': {
-			  'health': 9,
-			  'move': 3,
-			  'attack': 4,
-			  'range': 5,
-			  'attributes': [
-				'%shield% 2'
-			  ]
+			{
+				level: 7,
+				type: 'normal',
+				health: 1,
+				range: 1,
 			},
-			'elite': {
-			  'health': 12,
-			  'move': 3,
-			  'attack': 6,
-			  'range': 6,
-			  'attributes': [
-				'%pierce% 3',
-				'%shield% 2'
-			  ]
-			}
-		  },
-		  {
-			'level': 7,
-			'normal': {
-			  'health': 10,
-			  'move': 3,
-			  'attack': 4,
-			  'range': 6,
-			  'attributes': [
-				'%shield% 2'
-			  ]
+			{
+				level: 0,
+				type: 'elite',
+				health: 2,
+				attack: 1,
+				range: 1,
 			},
-			'elite': {
-			  'health': 13,
-			  'move': 3,
-			  'attack': 6,
-			  'range': 7,
-			  'attributes': [
-				'%pierce% 3',
-				'%shield% 3'
-			  ]
-			}
-		  }
-		]
-	  },
-	  'City Guard': {
-		'level': [
-		  {
-			'level': 0,
-			'normal': {
-			  'health': 5,
-			  'move': 2,
-			  'attack': 2,
-			  'range': 0,
-			  'attributes': []
+			{
+				level: 1,
+				type: 'elite',
+				range: 1,
+				attributes: ['%pierce% 1', '%shield% 1'],
 			},
-			'elite': {
-			  'health': 6,
-			  'move': 2,
-			  'attack': 3,
-			  'range': 0,
-			  'attributes': [
-				'%shield% 1'
-			  ]
-			}
-		  },
-		  {
-			'level': 1,
-			'normal': {
-			  'health': 5,
-			  'move': 2,
-			  'attack': 2,
-			  'range': 0,
-			  'attributes': [
-				'%shield% 1'
-			  ]
+			{
+				level: 2,
+				type: 'elite',
+				health: 1,
+				attack: 1,
+				attributes: ['%pierce% 2', '%shield% 1'],
 			},
-			'elite': {
-			  'health': 6,
-			  'move': 2,
-			  'attack': 3,
-			  'range': 0,
-			  'attributes': [
-				'%shield% 2'
-			  ]
-			}
-		  },
-		  {
-			'level': 2,
-			'normal': {
-			  'health': 7,
-			  'move': 2,
-			  'attack': 2,
-			  'range': 0,
-			  'attributes': [
-				'%shield% 1'
-			  ]
+			{
+				level: 3,
+				type: 'elite',
+				health: 1,
+				move: 1,
+				attributes: ['%pierce% 2', '%shield% 2'],
 			},
-			'elite': {
-			  'health': 9,
-			  'move': 2,
-			  'attack': 3,
-			  'range': 0,
-			  'attributes': [
-				'%shield% 2'
-			  ]
-			}
-		  },
-		  {
-			'level': 3,
-			'normal': {
-			  'health': 8,
-			  'move': 2,
-			  'attack': 3,
-			  'range': 0,
-			  'attributes': [
-				'%shield% 1'
-			  ]
+			{
+				level: 4,
+				type: 'elite',
+				health: 2,
+				range: 1,
 			},
-			'elite': {
-			  'health': 9,
-			  'move': 2,
-			  'attack': 4,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 1',
-				'%shield% 2'
-			  ]
-			}
-		  },
-		  {
-			'level': 4,
-			'normal': {
-			  'health': 9,
-			  'move': 3,
-			  'attack': 3,
-			  'range': 0,
-			  'attributes': [
-				'%shield% 1'
-			  ]
+			{
+				level: 5,
+				type: 'elite',
+				health: 1,
+				attack: 1,
+				attributes: ['%pierce% 3', '%shield% 2'],
 			},
-			'elite': {
-			  'health': 10,
-			  'move': 3,
-			  'attack': 4,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 2',
-				'%shield% 2'
-			  ]
-			}
-		  },
-		  {
-			'level': 5,
-			'normal': {
-			  'health': 10,
-			  'move': 3,
-			  'attack': 3,
-			  'range': 0,
-			  'attributes': [
-				'%shield% 2'
-			  ]
+			{
+				level: 6,
+				type: 'elite',
+				health: 1,
+				move: 1,
+				attack: 1,
 			},
-			'elite': {
-			  'health': 12,
-			  'move': 3,
-			  'attack': 4,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 2',
-				'%shield% 3'
-			  ]
-			}
-		  },
-		  {
-			'level': 6,
-			'normal': {
-			  'health': 11,
-			  'move': 3,
-			  'attack': 4,
-			  'range': 0,
-			  'attributes': [
-				'%shield% 2'
-			  ]
+			{
+				level: 7,
+				type: 'elite',
+				health: 1,
+				range: 1,
+				attributes: ['%pierce% 3', '%shield% 3'],
 			},
-			'elite': {
-			  'health': 13,
-			  'move': 3,
-			  'attack': 5,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 3',
-				'%shield% 3'
-			  ]
-			}
-		  },
-		  {
-			'level': 7,
-			'normal': {
-			  'health': 13,
-			  'move': 3,
-			  'attack': 4,
-			  'range': 0,
-			  'attributes': [
-				'%shield% 2'
-			  ]
+		],
+	},
+	'City Guard': {
+		base: {
+			'health': 5,
+			'move': 2,
+			'attack': 2,
+			'range': 0,
+			'attributes': []
+		},
+		upgrades: [
+			{
+				level: 1,
+				type: 'normal',
+				attributes: ['%shield% 1'],
 			},
-			'elite': {
-			  'health': 14,
-			  'move': 3,
-			  'attack': 6,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 3',
-				'%shield% 3'
-			  ]
-			}
-		  }
-		]
-	  },
+			{
+				level: 2,
+				type: 'normal',
+				health: 2,
+			},
+			{
+				level: 3,
+				type: 'normal',
+				health: 1,
+				attack: 1,
+			},
+			{
+				level: 4,
+				type: 'normal',
+				health: 1,
+				move: 1,
+			},
+			{
+				level: 5,
+				type: 'normal',
+				health: 1,
+				attributes: ['%shield% 2'],
+			},
+			{
+				level: 6,
+				type: 'normal',
+				health: 1,
+				attack: 1,
+			},
+			{
+				level: 7,
+				type: 'normal',
+				health: 2,
+			},
+			{
+				level: 0,
+				type: 'elite',
+				health: 1,
+				attack: 1,
+				attributes: ['%shield% 1'],
+			},
+			{
+				level: 1,
+				type: 'elite',
+				attributes: ['%shield% 2'],
+			},
+			{
+				level: 2,
+				type: 'elite',
+				health: 3,
+			},
+			{
+				level: 3,
+				type: 'elite',
+				attack: 1,
+				attributes: ['%retaliate% 1', '%shield% 2'],
+			},
+			{
+				level: 4,
+				type: 'elite',
+				health: 1,
+				move: 1,
+				attributes: ['%retaliate% 2', '%shield% 2'],
+			},
+			{
+				level: 5,
+				type: 'elite',
+				health: 2,
+				attributes: ['%retaliate% 2', '%shield% 3'],
+			},
+			{
+				level: 6,
+				type: 'elite',
+				health: 1,
+				attack: 1,
+				attributes: ['%retaliate% 3', '%shield% 3'],
+			},
+			{
+				level: 7,
+				type: 'elite',
+				health: 1,
+				attack: 1,
+			},
+		],
+	},
 };
