3742a26d0b01fd380ac4b58ea456d375bd182f4f
SamGraber <authorsamgraber@gmail.com>
2018-06-21 16:49:00 -0400

    Convert beasts and harrowers
    Normalizing data

--- src/data/gloomhaven/harrowers.data.ts
+++ src/data/gloomhaven/harrowers.data.ts
@@ -1,170 +1,101 @@
 export const Harrowers = {
 	'Harrower Infester': {
-		'level': [
-		  {
-			'level': 0,
-			'normal': {
-			  'health': 6,
-			  'move': 2,
-			  'attack': 2,
-			  'range': 0,
-			  'attributes': []
+		'base': {
+			'health': 6,
+			'move': 2,
+			'attack': 2,
+			'range': 0,
+			'attributes': []
+		},
+		'upgrades': [
+			{
+				'level': 0,
+				'type': 'elite',
+				'health': 6,
 			},
-			'elite': {
-			  'health': 12,
-			  'move': 2,
-			  'attack': 2,
-			  'range': 0,
-			  'attributes': []
-			}
-		  },
-		  {
-			'level': 1,
-			'normal': {
-			  'health': 7,
-			  'move': 2,
-			  'attack': 2,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 1'
-			  ]
+			{
+				'level': 1,
+				'type': 'normal',
+				'health': 1,
+				'attributes': ['%retaliate% 1']
 			},
-			'elite': {
-			  'health': 12,
-			  'move': 3,
-			  'attack': 2,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 2'
-			  ]
-			}
-		  },
-		  {
-			'level': 2,
-			'normal': {
-			  'health': 8,
-			  'move': 2,
-			  'attack': 2,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 2'
-			  ]
+			{
+				'level': 1,
+				'type': 'elite',
+				'move': 1,
+				'attributes': ['%retaliate% 2']
 			},
-			'elite': {
-			  'health': 14,
-			  'move': 3,
-			  'attack': 3,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 2'
-			  ]
-			}
-		  },
-		  {
-			'level': 3,
-			'normal': {
-			  'health': 10,
-			  'move': 2,
-			  'attack': 3,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 2'
-			  ]
+			{
+				'level': 2,
+				'type': 'normal',
+				'health': 1,
+				'attributes': ['%retaliate% 2']
 			},
-			'elite': {
-			  'health': 17,
-			  'move': 3,
-			  'attack': 3,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 3'
-			  ]
-			}
-		  },
-		  {
-			'level': 4,
-			'normal': {
-			  'health': 12,
-			  'move': 3,
-			  'attack': 3,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 2'
-			  ]
+			{
+				'level': 2,
+				'type': 'elite',
+				'health': 2,
+				'attack': 1,
 			},
-			'elite': {
-			  'health': 19,
-			  'move': 3,
-			  'attack': 4,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 3'
-			  ]
-			}
-		  },
-		  {
-			'level': 5,
-			'normal': {
-			  'health': 12,
-			  'move': 3,
-			  'attack': 4,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 3'
-			  ]
+			{
+				'level': 3,
+				'type': 'normal',
+				'health': 2,
+				'attack': 1,
 			},
-			'elite': {
-			  'health': 21,
-			  'move': 3,
-			  'attack': 5,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 3'
-			  ]
-			}
-		  },
-		  {
-			'level': 6,
-			'normal': {
-			  'health': 15,
-			  'move': 3,
-			  'attack': 4,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 3'
-			  ]
+			{
+				'level': 3,
+				'type': 'elite',
+				'health': 3,
+				'attributes': ['%retaliate% 3']
 			},
-			'elite': {
-			  'health': 22,
-			  'move': 4,
-			  'attack': 5,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 4'
-			  ]
-			}
-		  },
-		  {
-			'level': 7,
-			'normal': {
-			  'health': 17,
-			  'move': 3,
-			  'attack': 4,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 4'
-			  ]
+			{
+				'level': 4,
+				'type': 'normal',
+				'health': 2,
+				'move': 1,
 			},
-			'elite': {
-			  'health': 26,
-			  'move': 4,
-			  'attack': 5,
-			  'range': 0,
-			  'attributes': [
-				'%retaliate% 4'
-			  ]
+			{
+				'level': 4,
+				'type': 'elite',
+				'health': 2,
+				'attack': 1,
+			},
+			{
+				'level': 5,
+				'type': 'normal',
+				'attack': 1,
+				'attributes': ['%retaliate% 3']
+			},
+			{
+				'level': 5,
+				'type': 'elite',
+				'health': 2,
+				'attack': 1,
+			},
+			{
+				'level': 6,
+				'type': 'normal',
+				'health': 3,
+			},
+			{
+				'level': 6,
+				'type': 'elite',
+				'health': 1,
+				'move': 1,
+				'attributes': ['%retaliate% 4']
+			},
+			{
+				'level': 7,
+				'type': 'normal',
+				'health': 2,
+				'attributes': ['%retaliate% 4']
+			},
+			{
+				'level': 7,
+				'type': 'elite',
+				'health': 4,
 			}
-		  }
 		]
-	  },
+	}
 };
